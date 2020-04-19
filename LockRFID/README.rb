2970597dcf98fc7cdf808a3f8325d7430c72cb80
RudInc <62099876+RudInc@users.noreply.github.com>
2020-04-19 15:13:06 +0300

    Create README.rb
    add readme

---
+++ LockRFID/README.rb
@@ -0,0 +1,7 @@
+Arduino Projects. Smart Home
+
+Lock with keyboard and RFID mark.
+
+Main feature of this project is no library for keyboard. 
+Lock use only libraries for RFID. I dont use library for keyboard because I have been creating project for Arduino NANO. 
+Libraries for keyboard doesn't work properly on this chip. So I have written my program for matrix keyboard 4x4
